42bff3a41a1106b3204a5a7521011f40bbc22a8a
frank <frankfarfan96@gmail.com>
2024-06-09 15:41:59 +0200

    implemented room and BookedRoom models

--- src/main/java/org/farfenix/model/BookedRoom.java
+++ src/main/java/org/farfenix/model/BookedRoom.java
@@ -1,4 +1,66 @@
 package org.farfenix.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+    @Column(name = "check_In")
+    private LocalDate checkInDate;
+    @Column(name = "check_Out")
+    private LocalDate checkOutDate;
+    @Column(name = "guest_FullName")
+    private String guestFullName;
+    @Column(name = "guest_Email")
+    private String guestEmail;
+    @Column(name = "adults")
+    private int NumOfAdults;
+    @Column(name = "children")
+    private int NumOfChildren;
+    @Column(name = "total_guest")
+    private int totalNumGuest;
+    @Column(name = "confirmation_Code")
+    private String bookingConfirmationCode;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+
+
+
+
+
+
+
+    public void calculateTotalNumberOfGuest() {
+        this.totalNumGuest = this.NumOfAdults + this.NumOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        NumOfAdults = numOfAdults;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        NumOfChildren = numOfChildren;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
--- src/main/java/org/farfenix/model/Room.java
+++ src/main/java/org/farfenix/model/Room.java
@@ -1,4 +1,49 @@
 package org.farfenix.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked = false;
+    @Lob
+    private Blob photo;
+
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+    public Room() {
+        this.bookings = new ArrayList<>();
+    }
+
+
+    public void addBooking(BookedRoom booking) {
+        if (bookings == null) {
+            bookings = new ArrayList<>();
+        }
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked = true;
+        String bookingCode = RandomStringUtils.random(10);
+        booking.setBookingConfirmationCode(bookingCode);
+    }
 }
