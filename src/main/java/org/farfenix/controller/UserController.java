30ab9c216a686aaeb8bb6ba698c19bbafd727365
frank <frankfarfan96@gmail.com>
2024-07-03 20:59:40 +0200

    created class for security config and implemented userCon with first 3 apis

---
+++ src/main/java/org/farfenix/controller/UserController.java
@@ -0,0 +1,49 @@
+package org.farfenix.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.farfenix.model.User;
+import org.farfenix.service.IUserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final IUserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(userService.gerUsers(), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<?> getUserByEmail(@PathVariable("email") String email) {
+        try {
+            User theUser = userService.getUser(email);
+
+            return ResponseEntity.ok(theUser);
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching user");
+        }
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") String email) {
+        try {
+            userService.deleteUser(email);
+
+            return ResponseEntity.ok("User delete successfully");
+        }  catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting user");
+        }
+    }
+}
