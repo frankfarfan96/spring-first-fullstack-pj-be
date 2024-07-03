6acb0cf00ccf050cd88059e38c48cd25e644ebf4
frank <frankfarfan96@gmail.com>
2024-07-03 18:50:00 +0200

    created entity User and Role

---
+++ src/main/java/org/farfenix/model/Role.java
@@ -0,0 +1,44 @@
+package org.farfenix.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users = new HashSet<>();
+
+    public void assingRoleToUser(User user) {
+        user.getRoles().add(this);
+        this.getUsers().add(user);
+    }
+    public void removeUserFromRole(User user) {
+        user.getRoles().remove(this);
+        this.getUsers().remove(user);
+    }
+
+    public void removeAllUsersFromRole() {
+        if (this.getUsers() != null) {
+            List<User> roleUsers = this.getUsers().stream().toList();
+
+            roleUsers.forEach(this::removeUserFromRole);
+        }
+    }
+
+    public String getName() {
+        return name != null ? name : "";
+    }
+}
