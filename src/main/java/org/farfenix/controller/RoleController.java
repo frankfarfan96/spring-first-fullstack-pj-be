98067696973fad9207e77f912bb2e9fb99a77361
frank <frankfarfan96@gmail.com>
2024-07-05 08:42:52 +0200

    implemented roles apis

---
+++ src/main/java/org/farfenix/controller/RoleController.java
@@ -0,0 +1,58 @@
+package org.farfenix.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.farfenix.exception.RoleAlreadyExistException;
+import org.farfenix.model.Role;
+import org.farfenix.model.User;
+import org.farfenix.service.IRoleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/api/roles")
+public class RoleController {
+    private final IRoleService roleService;
+
+    @GetMapping("/all-roles")
+    public ResponseEntity<List<Role>> getAllRoles() {
+        return new ResponseEntity<>(roleService.getRoles(), HttpStatus.FOUND);
+    }
+
+    @PostMapping("/create-new-role")
+    public ResponseEntity<String> createRole(@RequestBody Role theRole) {
+        try {
+            roleService.createRole(theRole);
+
+            return ResponseEntity.ok("New role created successfully!");
+        } catch (RoleAlreadyExistException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/delete/{roleId}")
+    public void deleteRole(@PathVariable("roleId") Long roleId) {
+        roleService.deleteRole(roleId);
+    }
+
+    @PostMapping("/remove-all-users-from-role/{roleId}")
+    public Role removeAllUsersFromRole(@PathVariable("roleId") Long roleId) {
+        return roleService.removeAllUserFromRole(roleId);
+    }
+
+    @PostMapping("/remove-user-from-role")
+    public User removeUserFromRole(@RequestParam("userId") Long userId,@RequestParam("roleId") Long roleId) {
+        return roleService.removeUserFromRole(userId, roleId);
+    }
+
+    @PostMapping("/assign-user-to-role")
+    public User assignRoleToUser(@RequestParam("userId") Long userId,@RequestParam("roleId") Long roleId) {
+        return roleService.assignRoleToUser(userId, roleId);
+    }
+
+}
